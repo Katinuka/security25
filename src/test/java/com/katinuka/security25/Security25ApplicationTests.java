f9971b2b599d0c20e76b76765c0acffcba6cb015
Katinuka <9a523ab@mailfence.com>
2025-03-18 13:06:16 +0200

    Initial commit

---
+++ src/test/java/com/katinuka/security25/Security25ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.katinuka.security25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Security25ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
