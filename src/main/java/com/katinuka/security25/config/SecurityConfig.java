07c7ee0284d3424d8572c7f3e3b1991b810a8b07
Katinuka <9a523ab@mailfence.com>
2025-03-31 12:58:00 +0300

    basic authorization (csrf bypass)

---
+++ src/main/java/com/katinuka/security25/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.katinuka.security25.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+/*
+ @author    Katinuka
+ @project   security25
+ @version   1.0.0
+ @since     31-Mar-25
+ 
+ @see https://github.com/Katinuka/security25
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
